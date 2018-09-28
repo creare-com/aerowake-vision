5aa0e3a97cbdb60f8047967dd82e3456d44eddf7
draper <draper@mit.edu>
2018-09-28 17:19:25 -0400

    calibrate.launch calls my_camera.launch in the pointgrey driver. my_camera.launch sets the camera_info_url to creare/params/camera_calib.yaml. Calling roslaunch creare calibrate.launch will edit creare/params/camera_calib.yaml with the new values.

--- scripts/copy-calib-to-aerowake.sh
+++ scripts/copy-calib-to-aerowake.sh
@@ -2,5 +2,4 @@
 
 DATETIME=$(date +%Yy-%mm-%dd-%Hh-%Mm-%Ss)
 
-cp ~/.ros/camera_info/0.yaml ~/creare_ws/src/creare/params/camera_calib.yaml
-cp ~/.ros/camera_info/0.yaml ~/logs/camera/camera_calib_$DATETIME.yaml
+cp ~/creare_ws/src/creare/params/camera_calib.yaml ~/logs/camera/camera_calib_$DATETIME.yaml
