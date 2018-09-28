fa87b3de36e7c9deae1f1ae8fcbe19be7924b773
draper <draper@mit.edu>
2018-09-28 17:44:33 -0400

    Added image rectification since we must rectify before finding centroids.

--- scripts/centroid_finder.py
+++ scripts/centroid_finder.py
@@ -4,7 +4,7 @@ import rospy
 import numpy as np
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from creare.msg import Centroids, SolutionBounds
 
 class text_colors:
@@ -25,29 +25,30 @@ class text_colors:
   BOLD = '\033[1m'
   UNDERLINE = '\033[4m'
 
-class ImageProcessor(object):
+class CentroidFinder(object):
   def __init__(self, show_images = False):
     self._img = None
     self._roi = [0, 0, 0, 0, False]
     self._show_images = show_images
+    cam_info = rospy.wait_for_message("/camera/camera_info",CameraInfo)
+    self._mtx = np.array([cam_info.K[0:3],cam_info.K[3:6],cam_info.K[6:9]])
+    self._distortion = np.array(cam_info.D)
 
     # Image converter
     self._bridge = CvBridge()
 
     # Publishers
-    self.pub_initial_contours = rospy.Publisher("/initial_contours",Centroids,queue_size=1)
+    self.pub_initial_contours = rospy.Publisher("/initial_centroids",Centroids,queue_size=1)
 
     # Subscribers
     self.sub_img_raw = rospy.Subscriber("/camera/image_raw",Image,self.cbImgRaw)
     self.sub_prev_sol_bounds = rospy.Subscriber("/prev_solution_bounds",SolutionBounds,self.cbPrevSolBounds)
 
   def rectify(self):
-    # Undistortion and cropping
+    # Undistortion
     h,w = self._img.shape[:2]
-    mtx_new,roi = cv2.getOptimalNewCameraMatrix(mtx,distortion,(w,h),1,(w,h))
-    img_axes = cv2.undistort(img_axes,mtx,distortion,None,mtx_new)
-    x,y,w,h = roi
-    img_axes = img_axes[y:y+h, x:x+w]    
+    mtx_new,roi = cv2.getOptimalNewCameraMatrix(self._mtx,self._distortion,(w,h),1,(w,h))
+    self._img = cv2.undistort(self._img,self._mtx,self._distortion,None,mtx_new)
 
   def cbPrevSolBounds(self,data):
     '''
@@ -78,7 +79,7 @@ class ImageProcessor(object):
     self._img = self._img[buff:self._img.shape[0] - buff, buff:self._img.shape[1] - buff]
 
     # Rectify the image before finding contours
-    self._img = rectify(self._img)
+    self.rectify()
 
     # Find centroids and reshape for ROS publishing
     centroids = self.obtain_initial_centroids()
@@ -191,7 +192,7 @@ if __name__ == "__main__":
   rospy.init_node('centroid_finder')
 
   # Create the node
-  node = ImageProcessor(show_images = False)
+  node = CentroidFinder(show_images = False)
 
   # Spin
   rospy.spin()
